519ad062300ad8cc9bd62b3515e1b73198785267
billwu101 <bill.tpe101@gmail.com>
2026-04-06 15:27:56 +0800

    LowpassFilters_3 Revise

--- GaussianLowpassFilters_3/GaussianLowpass.m
+++
@@ -1,8 +0,0 @@
-close all; clear; clc;
-
-% Read all images in current folder
-fileList = [dir('*.jpeg'); dir('*.jpg'); dir('*.png'); dir('*.tiff'); dir('*.tif'); dir('*.bmp')];
-
-for f = 1:length(fileList)
-    processImage(fileList(f).name);
-end
